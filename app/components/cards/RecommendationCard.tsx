9615c0045a5f3525f13389e4cc57d81e9fb8c7f5
Ahmedkadrymostafa <ahmedkadry730@gmail.com>
2024-02-17 13:37:26 +0200

    fifth commit

--- app/components/cards/RecommendationCard.tsx
+++ app/components/cards/RecommendationCard.tsx
@@ -5,7 +5,7 @@ import { TiStarFullOutline } from "react-icons/ti";
 const RecommendationCard = (props: any) => {
   return (
     <div>
-        <div className="relative p-5 main-bg mt-6 mb-9 h-60 max-[768px]:w-full">
+        <div className="relative p-5 main-bg mt-6 mb-9 h-[16rem] max-[768px]:w-full">
                 <div className='absolute -top-6 right-8'>
                     <Image className='rounded-full' src={props.img} width={85} height={83} alt='rater image' />
                 </div>
@@ -15,7 +15,7 @@ const RecommendationCard = (props: any) => {
                         <h1 className='capitalize white mb-0.5'>{props.name}</h1>
                         <p className='capitalize light-gray text-xs'>{props.job}</p>
                     </div>
-                    <p className='light-gray text-sm mb-4 leading-5 line-clamp-5 text-ellipsis'>{props.message}</p>
+                    <p className='light-gray text-sm mb-4 leading-5 line-clamp-6 text-ellipsis'>{props.message}</p>
                   </div>
                   <div className='dark-bg flex py-1 px-4 rounded-2xl w-fit'>
                       
