5c55ca13791eebf12b6b3e8d885b8594ac791cb3
Ahmedkadrymostafa <ahmedkadry730@gmail.com>
2024-03-06 22:49:59 +0200

    firebase commit

--- app/firebase/firebase.tsx
+++ app/firebase/firebase.tsx
@@ -11,13 +11,10 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAh8H-Fd0QkHOzkDlJmzMRlpNcdfhLGBMA",
-  // apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: "my-portfolio-e2a8e",
-  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: "my-portfolio-e2a8e.appspot.com",
-  // storageBucket: process.env.STORAGE_BUCKET,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
